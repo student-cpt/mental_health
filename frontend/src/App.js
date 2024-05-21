31a4b31c23659a3b7a433167d28e308f7ff1d8a6
Manan Hingorani <mahijee1603@gmail.com>
2024-05-21 17:37:51 +0530

    updated push

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,19 @@ import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Navbar from './compnents/navbar/Navbar';
 import Home from './compnents/home/Home';
-import Signup from './compnents/SignupIn/Signup';
 import Profile from './compnents/profile/Profile';
 import NotFound from './compnents/notFound/NotFound';
+import Signup from './compnents/SignupIn/Signup';
 
 function App() {
   return (
     <BrowserRouter>
           <Routes>
+            <Route path='/:username/home' element={<Home />} />
+            <Route path='/:username/profile' element={<Profile />} />
+            <Route path='*' element={<NotFound />} />
             <Route path='/home' element={<Home />} />
+            <Route path='/signup' element={<Signup />} />
           </Routes>
       </BrowserRouter>
   );
