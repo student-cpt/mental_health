870cca01bd89d89f10c7240db5704282febbf43b
Shreya Ganjoo <98229024+Shreyg-27@users.noreply.github.com>
2024-05-21 17:29:36 +0530

    Signup page implemented without cancel functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Navbar from './compnents/navbar/Navbar';
 import Home from './compnents/home/Home';
+import Signup from './compnents/SignupIn/Signup';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Navbar />
           <Routes>
             <Route path='/home' element={<Home />} />
+            <Route path='/signup' element={<Signup />} />
           </Routes>
       </BrowserRouter>
   );
